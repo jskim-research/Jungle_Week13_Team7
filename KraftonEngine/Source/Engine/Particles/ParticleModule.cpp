f9224ee3af3ca70e369e8e27e08eae36348ec044
jskim-research <wltnwkd21@gmail.com>
2026-05-23 11:21:16 +0900

    Feat: add basic Particle Module class

---
+++ KraftonEngine/Source/Engine/Particles/ParticleModule.cpp
@@ -0,0 +1,65 @@
+﻿#include "ParticleModule.h"
+
+const FTransform& UParticleModule::FContext::GetTransform() const
+{
+	return FTransform();
+}
+
+UObject* UParticleModule::FContext::GetDistributionData() const
+{
+	return nullptr;
+}
+
+FString UParticleModule::FContext::GetTemplateName() const
+{
+	return FString();
+}
+
+FString UParticleModule::FContext::GetInstanceName() const
+{
+	return FString();
+}
+
+void UParticleModule::Spawn(const FSpawnContext& Context)
+{
+}
+
+void UParticleModule::Update(const FUpdateContext& Context)
+{
+}
+
+void UParticleModule::FinalUpdate(const FUpdateContext& Context)
+{
+}
+
+uint32 UParticleModule::RequiredBytes(UParticleModuleTypeDataBase* TypeData)
+{
+	return uint32();
+}
+
+uint32 UParticleModule::RequiredBytesPerInstance()
+{
+	return uint32();
+}
+
+void UParticleModule::GetCurveObjects(TArray<FParticleCurvePair>& OutCurves)
+{
+}
+
+void UParticleModule::RefreshModule()
+{
+}
+
+EModuleType UParticleModule::GetModuleType() const
+{
+	return EModuleType();
+}
+
+void UParticleModule::PostEditChangeProperty(const FPropertyChangedEvent& Event)
+{
+}
+
+bool UParticleModule::IsValidForLODLevel(UParticleLODLevel* LODLevel, FString& OutErrorString)
+{
+	return false;
+}
